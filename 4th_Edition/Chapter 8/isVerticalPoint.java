3e9f07a9c9d94124d77937033a75194d283abe9b
Michael "Misha" Ward <wardm5@uw.edu>
2019-06-20 15:48:32 -0700

    Create isVerticalPoint.java
    Returns true if the given Point lines up vertically with this Point; that is, if their x coordinates are the same.

---
+++ 4th_Edition/Chapter 8/isVerticalPoint.java
@@ -0,0 +1,5 @@
+//  Returns true if the given Point lines up vertically with this Point; that is, if their x coordinates are the same.
+
+public boolean isVertical(Point other) {
+    return (other.x == this.x);
+}
