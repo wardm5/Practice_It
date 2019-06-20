cb969c3ae3ab0159746e254d011704f58422025f
Michael "Misha" Ward <wardm5@uw.edu>
2019-06-19 18:55:56 -0700

    Create allLess.java
    Method that determines if the first of two int arrays has values less than the second int array at the same index. If lengths are not the same, then return false.

---
+++ 4th_Edition/Chapter 7/allLess.java
@@ -0,0 +1,9 @@
+public boolean allLess(int[] arr1, int[] arr2) {
+    if (arr1.length != arr2.length)
+        return false;
+    for(int i = 0; i < arr1.length; i++) {
+        if (arr1[i] >= arr2[i]) 
+            return false;
+    }
+    return true;
+}
