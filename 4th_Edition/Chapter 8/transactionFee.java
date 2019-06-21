489ce9d3237405c26a209a7b2462a55d6e1522f8
Michael "Misha" Ward <wardm5@uw.edu>
2019-06-21 09:58:53 -0700

    Create transactionFee.java
    https://practiceit.cs.washington.edu/problem/view/bjp4/chapter8/e11-transactionFeeBankAccount

---
+++ 4th_Edition/Chapter 8/transactionFee.java
@@ -0,0 +1,18 @@
+//  https://practiceit.cs.washington.edu/problem/view/bjp4/chapter8/e11-transactionFeeBankAccount
+
+public boolean transactionFee(double fee){
+    int trans = transactions;
+    double totalFee = 0.0;
+    double bal = balance;
+    if (trans == 0) 
+        return false;
+    for(int i = 1; i <= trans; i++){
+        totalFee += fee*i;
+        if (totalFee >= bal) {
+            balance = 0.0;
+            return false;
+        }   
+    }
+    balance = bal - totalFee;
+    return true;  
+}
