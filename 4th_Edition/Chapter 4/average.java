e984a0c5b12943582ccb247bea99a5a0f401808d
Michael "Misha" Ward <wardm5@uw.edu>
2019-06-24 18:00:53 -0700

    Create average.java
    Write a method called average that takes two integers as parameters and returns the average of the two integers.

---
+++ 4th_Edition/Chapter 4/average.java
@@ -0,0 +1,5 @@
+//  Write a method called average that takes two integers as parameters and returns the average of the two integers.
+
+public double average(int a, int b) {
+    return (double) (a + b) / 2.0;
+}
