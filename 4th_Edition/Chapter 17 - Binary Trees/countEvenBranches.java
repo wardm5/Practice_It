d32ae7748a38ad0e27febd6e038560b8606b6ba1
Michael "Misha" Ward <wardm5@uw.edu>
2019-06-25 08:19:43 -0700

    Create countEvenBranches.java
    Write a method countEvenBranches that returns the number of branch nodes in a binary tree that contain even numbers.

---
+++ 4th_Edition/Chapter 17 - Binary Trees/countEvenBranches.java
@@ -0,0 +1,32 @@
+//  Write a method countEvenBranches that returns the number of branch nodes in a binary tree that contain even numbers.
+
+public int countEvenBranches() {
+    if (overallRoot == null ) { 
+        return 0;
+    }
+    int count = 0;
+    Queue<IntTreeNode> q = new LinkedList<>();
+    q.add(overallRoot);
+    while(!q.isEmpty()) {
+        IntTreeNode temp = q.remove();
+        if (temp.data % 2 == 0 && (temp.left != null || temp.right != null) )
+            count++;
+        if (temp.left != null) {
+            q.add(temp.left);
+        }
+        if (temp.right != null) {
+            q.add(temp.right);
+        }
+    }
+    return count;
+//    return countEvenBranches(overallRoot);
+}
+
+//int countEvenBranches(IntTreeNode root) {
+//    int count = 0;
+//    if (root == null) {
+//        return 0;
+//    } else if (root.data % 2 == 0 && (root.left != null || root.right !=null)) {
+//        count++;
+//    }
+//}
