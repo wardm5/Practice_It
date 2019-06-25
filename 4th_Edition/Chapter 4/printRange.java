74608adc485564262e4b1a050687e5d3d2f9ee7c
Michael "Misha" Ward <wardm5@uw.edu>
2019-06-24 17:05:09 -0700

    Create printRange.java
    Write a method called printRange that accepts two integers as arguments and prints the sequence of numbers between the two arguments, separated by spaces.

---
+++ 4th_Edition/Chapter 4/printRange.java
@@ -0,0 +1,13 @@
+//  Write a method called printRange that accepts two integers as arguments and prints the sequence of numbers between the two arguments, separated by spaces. 
+
+public void printRange(int a, int b) {
+    if(a <= b) {
+         for (int i = a; i <= b; i++) {
+             System.out.print(i + " ");
+         }
+    } else {
+         for (int i = b; i <= a; i--) {
+             System.out.print(i + " ");
+         }
+    }
+}
