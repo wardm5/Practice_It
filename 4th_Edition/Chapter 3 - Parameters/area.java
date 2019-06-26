36c83320f8d3ebcfc412f0b2f31a69a910393c95
Michael "Misha" Ward <wardm5@uw.edu>
2019-06-26 10:13:54 -0700

    Create area.java
    Write a method named area that accepts the radius of a circle as a parameter and returns the area of a circle with that radius.

---
+++ 4th_Edition/Chapter 3 - Parameters/area.java
@@ -0,0 +1,5 @@
+//  Write a method named area that accepts the radius of a circle as a parameter and returns the area of a circle with that radius.
+
+public double area(double r) {
+    return Math.PI * r * r;
+}
