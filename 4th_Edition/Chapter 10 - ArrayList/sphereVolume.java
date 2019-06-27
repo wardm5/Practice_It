223ef80a961a073e9e27a2355dd75b0ebecae2b6
Michael "Misha" Ward <wardm5@uw.edu>
2019-06-26 17:02:30 -0700

    Create sphereVolume.java
    Write a method called sphereVolume that accepts a radius (a real number) as a parameter and returns the volume of a sphere with that radius.

---
+++ 4th_Edition/Chapter 10 - ArrayList/sphereVolume.java
@@ -0,0 +1,5 @@
+//  Write a method called sphereVolume that accepts a radius (a real number) as a parameter and returns the volume of a sphere with that radius.
+
+public double sphereVolume(double r) {
+    return (double)4/3*Math.PI*r*r*r;
+}
