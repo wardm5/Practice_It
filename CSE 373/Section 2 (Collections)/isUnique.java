f6f1befdfaa72b6efdfa13f16dcfd62433f00d15
Misha Ward <wardm5@uw.edu>
2019-07-09 22:13:57 -0700

    Create isUnique.java

---
+++ CSE 373/Section 2 (Collections)/isUnique.java
@@ -0,0 +1,21 @@
+/*
+    Write a method isUnique that accepts a Map from strings to strings as a parameter and returns true if no two keys map 
+    to the same value (and false if any two or more keys do map to the same value). For example, calling your method on the 
+    following map would return true:
+
+    {Marty=Stepp, Stuart=Reges, Jessica=Miller, Amanda=Camp, Hal=Perkins}
+    Calling it on the following map would return false, because of two mappings for Perkins and Reges:
+
+    {Kendrick=Perkins, Stuart=Reges, Jessica=Miller, Bruce=Reges, Hal=Perkins}
+    The empty map is considered to be unique, so your method should return true if passed an empty map.
+ */
+
+public boolean isUnique(Map<String, String> map) {
+    HashSet<String> set = new HashSet<>();
+    for (String value : map.values()) {
+        if (set.contains(value))
+            return false;
+        set.add(value);
+    }
+    return true;  
+}
