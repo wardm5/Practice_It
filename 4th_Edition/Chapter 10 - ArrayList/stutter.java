7c2e369cb6be8aa68422ee12ccc58f53a9d7626b
Misha Ward <wardm5@uw.edu>
2019-07-15 23:55:02 -0700

    Update stutter.java

--- 4th_Edition/Chapter 10 - ArrayList/stutter.java
+++ 4th_Edition/Chapter 10 - ArrayList/stutter.java
@@ -1,17 +1,17 @@
 //  Write a method stutter that takes an ArrayList of Strings and an integer k as parameters and that replaces every string with k copies of that string. 
 
 void stutter(ArrayList<String> list, int k) {
-    if (k <= 0) {
+    if (k <= 0) {   // if k is 0, then clear the list...
         list.clear();
         return;
     }
-    if (k == 1) {
+    if (k == 1) {  // if k is 1, return the item
         return;
     }
-    for (int i = 0; i < list.size(); i++) {
-        for (int j = 1; j < k; j++) {
-            list.add(i, list.get(i));
-            i++;
+    for (int i = 0; i < list.size(); i++) {   // else if k is greater, for all items in the list
+        for (int j = 1; j < k; j++) {  // repeat the item k amount of times
+            list.add(i, list.get(i));  // input the value where the index i is set
+            i++;                       // increment i for each added value
         }
     }
 }
