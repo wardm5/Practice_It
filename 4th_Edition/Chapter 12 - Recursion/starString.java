f1513922460bb62dff1db0ea9f99759e97a28c28
Misha Ward <wardm5@uw.edu>
2019-08-11 21:31:46 -0700

    Update starString.java

--- 4th_Edition/Chapter 12 - Recursion/starString.java
+++ 4th_Edition/Chapter 12 - Recursion/starString.java
@@ -17,3 +17,13 @@ public StringBuilder starStringHelper(int n) {
     newSb.append(sb);
     return newSb;
 }
+
+
+public String starString(int n) {
+    if (n < 0)
+        throw new IllegalArgumentException();
+    if (n == 0)
+        return "*";
+    else
+        return starString(n-1) + starString(n-1);
+}
