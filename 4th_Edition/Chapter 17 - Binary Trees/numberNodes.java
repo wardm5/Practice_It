0d9c7985a6d31345a98956b254bd0925586f09cd
Misha Ward <wardm5@uw.edu>
2019-09-02 00:01:06 -0700

    Update numberNodes.java

--- 4th_Edition/Chapter 17 - Binary Trees/numberNodes.java
+++ 4th_Edition/Chapter 17 - Binary Trees/numberNodes.java
@@ -1,23 +1,37 @@
 //  Write a method numberNodes that changes the data stored in a binary tree, assigning sequential integers starting with 1 to each node so that a pre-order traversal will produce the numbers in order(1, 2, 3, etc.).
 
-int numberNodes() {
-    if (overallRoot == null) 
-        return 0;
-    Stack<IntTreeNode> s = new Stack<>();
-    s.add(overallRoot);
-    int size = 0;
-    int count = 1;
-    while (!s.isEmpty()) {
-        IntTreeNode temp = s.pop();
-        temp.data = count;
-        size++;
-        if (temp.right != null) {
-            s.add(temp.right);
-        }
-        if (temp.left != null) {
-            s.add(temp.left);
-        }
-        count++;
-    }
-    return size;
+// int numberNodes() {
+//     if (overallRoot == null) 
+//         return 0;
+//     Stack<IntTreeNode> s = new Stack<>();
+//     s.add(overallRoot);
+//     int size = 0;
+//     int count = 1;
+//     while (!s.isEmpty()) {
+//         IntTreeNode temp = s.pop();
+//         temp.data = count;
+//         size++;
+//         if (temp.right != null) {
+//             s.add(temp.right);
+//         }
+//         if (temp.left != null) {
+//             s.add(temp.left);
+//         }
+//         count++;
+//     }
+//     return size;
+// }
+
+int count = 0;
+public int numberNodes() {
+    helper(overallRoot, 1);
+    return count;
+}
+public int helper(IntTreeNode root, int n) {
+    if (root == null)
+        return n;
+    count++;
+    root.data = n;
+    int left = helper(root.left, n + 1);
+    return helper(root.right, left);
 }
