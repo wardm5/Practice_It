2a454809560e51d891779cb9a2f66eb5fb5c7489
Misha Ward <wardm5@uw.edu>
2019-09-05 20:54:30 -0700

    Update size.java

--- 4th_Edition/Chapter 17 - Binary Trees/size.java
+++ 4th_Edition/Chapter 17 - Binary Trees/size.java
@@ -10,3 +10,34 @@ public int helper(IntTreeNode root) {
     else 
         return helper(root.left) + helper(root.right) + 1;
 }
+
+public int size() {
+    if (overallRoot == null)
+        return 0;
+    else
+        return helper(overallRoot);
+}
+public int helper(IntTreeNode root) {
+    int count = 1;
+    Queue<IntTreeNode> q = new LinkedList<>();
+    q.add(root);
+//    System.out.print(root.data + " ");
+    while (!q.isEmpty()) {
+        int size = q.size();
+        for (int i = 0; i < size; i++) {
+            IntTreeNode temp = q.remove();
+            if (temp.left != null) {
+//                System.out.print(temp.left.data + " ");
+                q.add(temp.left);
+                count++;
+            }
+            if (temp.right != null) {
+//                System.out.print(temp.right.data + " ");
+                q.add(temp.right);
+                count++;
+            }
+        }
+        size = q.size();
+    }
+    return count;
+}
