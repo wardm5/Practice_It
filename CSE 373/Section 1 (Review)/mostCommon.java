342cf8eaf56bdb6c302d50e9b73d390193a70605
Misha Ward <wardm5@uw.edu>
2019-09-11 00:23:04 -0700

    Update mostCommon.java

--- CSE 373/Section 1 (Review)/mostCommon.java
+++ CSE 373/Section 1 (Review)/mostCommon.java
@@ -1,12 +1,12 @@
 public String mostCommon(String s) {
     HashMap<String, Integer> map = new HashMap<>();
+    s = s.trim();
     String[] arr = s.split("[ ]+");
     String res = "";
     int max = 0;
     for (int i = 0 ; i < arr.length; i++) {
-        int temp = map.getOrDefault(map.get(arr[i]), 0) + 1;
+        int temp = (map.getOrDefault(arr[i], 0) + 1);
         map.put(arr[i], temp);
-//        System.out.println(arr[i] + temp);
         if (temp > max){
             max = temp;
             res = arr[i];
