7a47e094c77662438104dd21e6d75c5a5f6551eb
Misha Ward <wardm5@uw.edu>
2019-09-24 01:19:10 -0700

    Create range.java

---
+++ 5th_Edition/Chapter 7: Arrays/range.java
@@ -0,0 +1,13 @@
+// Time Complexity: O(n), Space Complexity: O(1)
+
+public int range(int[] arr) {
+    int min = Integer.MAX_VALUE;
+    int max = Integer.MIN_VALUE;
+    for (int i = 0; i < arr.length; i++) {
+        if (arr[i] < min)
+            min = arr[i];
+        if (arr[i] > max)
+            max = arr[i];
+    }
+    return max - min + 1;
+}
