34c2cfb8f772a6e16ba90fe7deb29ea38ed2b5d9
Misha Ward <wardm5@uw.edu>
2019-09-25 13:14:00 -0700

    Create equals.java

---
+++ 5th_Edition/Chapter 7: Arrays/equals.java
@@ -0,0 +1,9 @@
+public boolean equals(String[] arr1, String[] arr2) {
+    if (arr1.length != arr2.length)
+        return false;
+    for (int i = 0; i < arr1.length; i++) {
+        if (!arr1[i].equals(arr2[i]))
+            return false;
+    }
+    return true;
+}
