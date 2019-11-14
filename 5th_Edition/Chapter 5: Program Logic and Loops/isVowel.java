b51d3ebe2ebbde731e739499e0d80533a49d51d5
Misha Ward <wardm5@uw.edu>
2019-11-13 22:34:52 -0800

    Create isVowel.java

---
+++ 5th_Edition/Chapter 5: Program Logic and Loops/isVowel.java
@@ -0,0 +1,6 @@
+public boolean isVowel(String a) {
+    return (a.toLowerCase().equals("a") || a.toLowerCase().equals("e")  
+                                   || a.toLowerCase().equals("i") 
+                                   || a.toLowerCase().equals("o")
+                                   || a.toLowerCase().equals("u"));
+}
